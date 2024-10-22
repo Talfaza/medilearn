94c7df1f83a01556b387e003e71512352ca0ce95
Talfaza <charifyahia4@gmail.com>
2024-10-22 16:32:47 +0100

    add: components

---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,134 @@
+<script setup lang="ts">
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu'
+
+const components: { title: string,  description: string }[] = [
+  {
+    title: 'Titre',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat ',
+
+  },
+
+
+
+  {
+    title: 'Titre',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat ',
+  },
+  {
+    title: 'Titre',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat ',
+  },
+]
+</script>
+
+<template>
+  <NavigationMenu>
+    <NavigationMenuList>
+      <NavigationMenuItem>
+        <NavigationMenuTrigger>Titre 1</NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <ul class="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[minmax(0,.75fr)_minmax(0,1fr)]">
+            <li class="row-span-3">
+              <NavigationMenuLink as-child>
+                <a
+                  class="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                >
+                  <div class="mb-2 mt-4 text-lg font-medium">
+                    Medilearn
+                  </div>
+                  <p class="text-sm leading-tight text-muted-foreground">
+                    Tout les cours des facultés de médecine a votre disposition !  
+                  </p>
+                </a>
+              </NavigationMenuLink>
+            </li>
+
+            <li>
+              <NavigationMenuLink as-child>
+                <a
+                  class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                >
+                  <div class="text-sm font-medium leading-none">Zone 1</div>
+                  <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat 
+                  </p>
+                </a>
+              </NavigationMenuLink>
+            </li>
+            <li>
+              <NavigationMenuLink as-child>
+                <a
+                  class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                >
+                  <div class="text-sm font-medium leading-none">Zone 2</div>
+                  <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat 
+                  </p>
+                </a>
+              </NavigationMenuLink>
+            </li>
+            <li>
+              <NavigationMenuLink as-child>
+                <a
+                  class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                >
+                  <div class="text-sm font-medium leading-none">Zone 3</div>
+                  <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat 
+                  </p>
+                </a>
+              </NavigationMenuLink>
+            </li>
+          </ul>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuTrigger>Titre 2</NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <ul class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+            <li v-for="component in components" :key="component.title">
+              <NavigationMenuLink as-child>
+                <a
+                  class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                >
+                  <div class="text-sm font-medium leading-none">{{ component.title }}</div>
+                  <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                    {{ component.description }}
+                  </p>
+                </a>
+              </NavigationMenuLink>
+            </li>
+          </ul>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+        Titre 3
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  <NavigationMenuItem>
+        <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 0.875C5.49797 0.875 3.875 2.49797 3.875 4.5C3.875 6.15288 4.98124 7.54738 6.49373 7.98351C5.2997 8.12901 4.27557 8.55134 3.50407 9.31167C2.52216 10.2794 2.02502 11.72 2.02502 13.5999C2.02502 13.8623 2.23769 14.0749 2.50002 14.0749C2.76236 14.0749 2.97502 13.8623 2.97502 13.5999C2.97502 11.8799 3.42786 10.7206 4.17091 9.9883C4.91536 9.25463 6.02674 8.87499 7.49995 8.87499C8.97317 8.87499 10.0846 9.25463 10.8291 9.98831C11.5721 10.7206 12.025 11.8799 12.025 13.5999C12.025 13.8623 12.2376 14.0749 12.5 14.0749C12.7623 14.075 12.975 13.8623 12.975 13.6C12.975 11.72 12.4778 10.2794 11.4959 9.31166C10.7244 8.55135 9.70025 8.12903 8.50625 7.98352C10.0187 7.5474 11.125 6.15289 11.125 4.5C11.125 2.49797 9.50203 0.875 7.5 0.875ZM4.825 4.5C4.825 3.02264 6.02264 1.825 7.5 1.825C8.97736 1.825 10.175 3.02264 10.175 4.5C10.175 5.97736 8.97736 7.175 7.5 7.175C6.02264 7.175 4.825 5.97736 4.825 4.5Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+        &nbsp;Authentification
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+
+  </NavigationMenu>
+
+</template>
+
+
+
+
